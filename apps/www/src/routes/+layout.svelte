f8cbbe10718cec41730064a8f091642d6c1b76e7
cybrejon <cybrejon.senpai@gmail.com>
2026-03-04 00:51:40 +0800

    chore(apps/www): add first (hero) section

---
+++ apps/www/src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import HeaderOrnament from "$components/header-ornament.svelte";
+  import Navbar from "$components/navbar.svelte";
+  import "@fontsource/michroma";
+  // 100-900 weight
+  import "@fontsource-variable/archivo";
+  import "../app.css";
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <title>Browser.icu</title>
+</svelte:head>
+
+<div class=" relative min-h-dvh space-y-12">
+  <!-- HEADER START -->
+  <div class="h-7 overflow-hidden">
+    <HeaderOrnament class="text-foreground-darker/80 -translate-x-4" />
+  </div>
+  <Navbar />
+  <!-- HEADER END -->
+
+  {@render children()}
+</div>
+
+<!-- NOISE OVERLAY -->
+<div class="site-noise-overlay pointer-events-none fixed inset-0 z-50"></div>
---
+++ apps/www/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Hero from "$components/sections/Hero.svelte";
+  import HeaderOrnament from "$components/header-ornament.svelte";
+</script>
+
+<Hero />
+<div class="border-border h-7 overflow-hidden border-b">
+  <HeaderOrnament class="text-foreground-darker/80 -translate-x-4" />
+</div>
