42fe368fe997096aa1be984e31c23601ce20846a
cybrejon <cybrejon.senpai@gmail.com>
2026-03-08 03:55:10 +0800

    chore(apps/www): add stats section

--- apps/www/src/routes/+page.svelte
+++ apps/www/src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
   import Hero from "$components/sections/Hero.svelte";
   import HeaderOrnament from "$components/header-ornament.svelte";
+  import Stats from "$components/sections/Stats.svelte";
 </script>
 
 <Hero />
+<Stats />
 <div class="border-border h-7 overflow-hidden border-b">
   <HeaderOrnament class="text-foreground-darker/80 -translate-x-4" />
 </div>
